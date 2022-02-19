7d05addfa9350bfac7b5506b0814d1ab47846a21
Renlagen <93928836+Renlagen@users.noreply.github.com>
2022-02-20 01:33:17 +0300

    Add files via upload

---
+++ PZ_13/PZ_13-2.py
@@ -0,0 +1,7 @@
+# 2.Из заданной строки отобразить только символы пунктуации. Использовать библиотеку string.
+# Строка: --msg-template="$FileDir$\{path}:{line}:{column}:{C}:({symbol}){msg}"
+import string
+
+a = '--msg-template="$FileDir$\{path}:{line}:{column}:{C}:({symbol}){msg}"'
+b = [i for i in a if i in string.punctuation]
+print('Результат:', *b)
